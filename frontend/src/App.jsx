575544b963450164f6ed510a75f174d13b6ae6cd
aakashchamola <aakashchamolababa@gmail.com>
2025-02-21 20:36:52 +0530

    title updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
       <h1 className="text-4xl font-extrabold text-blue-600 mb-8">
-        Data Catalyst: Intelligent API Processor
+      JSON Processor
       </h1>
       <div className="w-full max-w-lg">
         <InputForm setApiResponse={setApiResponse} />
